418fe7458be8cf70ec1a2eb74bb394910ced3ae0
matsumoto <matsumoto@basicinc.jp>
2016-07-10 14:09:17 +0900

    routes 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   post '/callback' => 'webhook#callback'
-  post '/lunch_cal' => 'webhook#lunch_cal'
+  get '/lunch_cal' => 'webhook#lunch_cal'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
